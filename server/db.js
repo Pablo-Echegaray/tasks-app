3306f4453a83131ac2ae898849278e5f3a027d83
Pablo <pabloechegaray95@gmail.com>
2022-09-18 18:50:42 -0300

    Conexion db

--- server/db.js
+++ server/db.js
@@ -1,4 +1,5 @@
 import {createPool} from 'mysql2/promise';
+//import db from 'mysql2/promise';
  
 export const pool = createPool({
     host: 'localhost',
@@ -6,5 +7,12 @@ export const pool = createPool({
     user: 'root',
     password: '584577',
     dataBase: 'tasksdb'
+}); 
+/*
+export const pool = db.configure({
+    "host": "localhost",
+    "user": "root",
+    "password": "584577",
+    "database": "tasksdb"
 });
-
+*/
--- server/index.js
+++ server/index.js
@@ -5,7 +5,9 @@ import taskRoutes from './routes/tasks.routes.js';
 
 
 const app = express();
+app.use(express.json());
 app.use(indexRoutes);
 app.use(taskRoutes);
 app.listen(PORT);
 console.log(`Server is running on port ${PORT}`);
+//npm run dev
